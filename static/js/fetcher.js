f0af64462be68aded6ef9f6b0a3f971b39c40e0b
Caleb Bray <caleb.bray@emsibg.com>
2023-07-12 14:32:51 -0700

    push

--- static/js/fetcher.js
+++ static/js/fetcher.js
@@ -7,13 +7,9 @@ function requestAIImage(params) {
     },
     body: JSON.stringify({
       key: "hPirRJ8gJ6KrtHfp0hhGC1B5D55f2BVb8BDQK6kgJ5qUA6eSL6o0J0wqwWaf",
-      prompt: `((${searchTerm})), action, white, caucasian, 1995, comedic, work, christian, holy, professional, jesus, family, pixar, hyper detail, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K, cell-shaded ${Math.floor(
-        Math.random() * 100
-      )}`,
-      negative_prompt:
-        "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((extra legs)), ((ugly)), anime",
+      prompt: `((${searchTerm}))`,
       width: "1080",
-      height: "720",
+      height: "360",
       samples: "1",
       num_inference_steps: "5",
       seed: 2,
@@ -34,8 +30,8 @@ function getSearchPrompt(params) {
 
 async function getThatThing(params) {
   const response = await requestAIImage(params);
-  if (response.status === "success") {
-    console.log(response.output);
+  if (response.status === "success" && response.output.length) {
+    console.log(response.output[0]);
     return response.output[0];
   }
 
