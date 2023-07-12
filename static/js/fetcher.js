9262eaa42dadc919feae357aae3c093ff7e00415
Thomas Black <thomas.black@emsibg.com>
2023-07-12 11:48:25 -0700

    try on caleb's machine

--- static/js/fetcher.js
+++ static/js/fetcher.js
@@ -1,7 +1,10 @@
 async function getThatThing() {
-  const response = fetch("https://stablediffusionapi.com/api/v3/text2img", {
+  const response = await fetch("https://stablediffusionapi.com/api/v3/text2img", {
     method: "POST",
-    body: {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
       key: "hPirRJ8gJ6KrtHfp0hhGC1B5D55f2BVb8BDQK6kgJ5qUA6eSL6o0J0wqwWaf",
       prompt:
         "ultra realistic ((chief executive)), comedic, goofy, funny, hyper detail, cinematic lighting, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
@@ -15,12 +18,44 @@ async function getThatThing() {
       guidance_scale: 7.5,
       webhook: null,
       track_id: null,
-    },
+    }),
   });
+
   const data = await response.json();
-  return data;
+  // console.log(`🧐 ~ res 1:`, data)
+
+  setTimeout(() => {
+    console.log("waited 5 seconds");
+  }, 5000);
+
+  const resultResponse = await fetch("https://stablediffusionapi.com/api/v3/text2img", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      key: "hPirRJ8gJ6KrtHfp0hhGC1B5D55f2BVb8BDQK6kgJ5qUA6eSL6o0J0wqwWaf",
+      prompt:
+        "ultra realistic ((chief executive)), comedic, goofy, funny, hyper detail, cinematic lighting, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
+      negative_prompt:
+        "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((extra legs)), anime",
+      width: "1080",
+      height: "720",
+      samples: "1",
+      num_inference_steps: "30",
+      seed: null,
+      guidance_scale: 7.5,
+      webhook: null,
+      track_id: null,
+    }),
+  });
+  console.log(resultResponse)
+  return resultResponse.json();
 }
 
 function logSomething() {
-  console.log("Nice job idiot");
+  getThatThing().then((data) => {
+    console.log("data");
+    return data;
+  });
 }
